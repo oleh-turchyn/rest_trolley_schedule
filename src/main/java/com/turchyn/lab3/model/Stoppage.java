3e96c46c3ff8e8b29e183d584ab79220ffc94ad2
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2021-02-27 18:47:32 +0200

    add project skeleton

---
+++ src/main/java/com/turchyn/lab3/model/Stoppage.java
@@ -0,0 +1,4 @@
+package com.turchyn.lab3.model;
+
+public class Stoppage {
+}
---
+++ src/test/java/com/turchyn/lab3/Lab3CrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turchyn.lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3CrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
